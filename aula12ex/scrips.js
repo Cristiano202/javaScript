c9c5d7cae3fde9cd275f705819fc732a817c4490
Cristiano202 <20cristianogoncalves20.22@gmail.com>
2025-04-10 19:16:14 -0300

    projeto atualisado

--- aula12ex/scrips.js
+++ aula12ex/scrips.js
@@ -6,13 +6,16 @@ function carregar(){
     msg.innerHTML=`Agora são ${hora} horas`
     if(hora>=0 && hora<12){
         foto.src='fotos/fotomanha.jpg'
+        document.body.style.background='#DBE2EA'
     }
     else if(hora >=12 && hora <18){
         foto.src='fotos/fototarde.jpg'
+        document.body.style.background='#C8834E'
     
     }
     else{
         foto.src='fotos/fotonoite.jpg'
+        document.body.style.background='#031A36'
     
     }
 }
