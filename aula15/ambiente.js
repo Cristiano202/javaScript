e9c9ecc456cb39cc3902ee40f5790596ce16c714
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-07-04 10:54:14 -0300

    Estudando javascript

---
+++ aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5,8,2,9,3]
+console.log(`Mostrando o vetor ${num}`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+num.sort()
+console.log(`Vetor ordenado ${num}`)
+console.log(`O vetor tem ${num.length} Posições`)
+num.push(1)
+console.log(num)
+let pos = num.indexOf(2)
+console.log(`O valor 8 está na posição ${pos}`)
---
+++ aula15/teste.js
@@ -0,0 +1,11 @@
+let vetor =[3,2,34,5,7,1]
+vetor.sort()
+console.log('Mostando vetor')
+for(let pos =0 ;pos<vetor.length;pos++){
+    console.log(vetor[pos])
+}
+ 
+console.log('Formula mais simples ')
+for(let pos in vetor){
+    console.log(vetor[pos])
+} 
